2d1e9be80509889307bf595565027d773537a0c9
George Albertyn <GeorgeA@abg.co.za>
2017-02-06 17:27:38 +0200

    Rate Controller Routes Updated
    Changed some routing attributes. Also MongoDB
    FilterDefinition<TDocument> for queries.

--- Controllers/RateController.cs
+++ Controllers/RateController.cs
@@ -30,6 +30,7 @@ namespace FixerDotNet.Controllers
 
             _database = _client.GetDatabase("ExchangeRates");
         }
+
         private async Task<Fixer> GetFixerAsync(string IsoAlpha3Code)
         {
             HttpResponseMessage response = await client.GetAsync("latest?base=" + IsoAlpha3Code);
@@ -40,7 +41,8 @@ namespace FixerDotNet.Controllers
             var collection = _database.GetCollection<Fixer>("Fixes");
             await collection.InsertOneAsync(fix);
         }
-        [HttpGet("api/Rate/Import")]
+
+        [HttpGet("api/Rates/Import")]
         public JsonResult ImportRates()
         {
             CommonResult result = new CommonResult();
@@ -55,42 +57,60 @@ namespace FixerDotNet.Controllers
             }
             return new JsonResult(fix);
         }
-        [HttpGet("api/Rates/Date/{Date:datetime}")]
-        public async Task<JsonResult> GetRatesByDateAsync(DateTime Date)
+
+
+
+        private async Task<List<Fixer>> ListFixesByDate(DateTime Date)
         {
             IMongoCollection<Fixer> collection = _database.GetCollection<Fixer>("Fixes");
             string _date = Date.ToString("yyyy-MM-dd");
             var result = collection.Find<Fixer>(x => x.date == _date);
-            List<Fixer> list = await result.ToListAsync<Fixer>();
-            return new JsonResult(list, new JsonSerializerSettings() { Formatting = Formatting.Indented });
+            return await result.ToListAsync<Fixer>();
         }
-        [HttpGet("api/Rates/Date/{Date:datetime}/Base/{}")]
-        public async Task<JsonResult> GetIsoRatesByDateAsync(string IsoAlpha3Code, DateTime Date)
+        [HttpGet("api/Rates/Date/{Date:datetime}")]
+        public async Task<JsonResult> GetRatesByDateAsync(DateTime Date)
+        {
+            return new JsonResult(await ListFixesByDate(Date), new JsonSerializerSettings() { Formatting = Formatting.Indented });
+        }
+        
+
+
+        private async Task<Fixer> FirstFixerByDateAndBase(DateTime Date, string Base)
         {
             IMongoCollection<Fixer> collection = _database.GetCollection<Fixer>("Fixes");
-            string _date = Date.ToString("yyyy-MM-dd");
-            var filter = Builders<Fixer>.Filter.Eq("date", _date);
-            Dictionary<string, object> list = new Dictionary<string, object>();
-            var count = 0;
-            using (var cursor = await collection.FindAsync(filter))
-            {
-                while (await cursor.MoveNextAsync())
-                {
-                    var batch = cursor.Current;
-                    foreach (var document in batch)
-                    {
-                        // process document
-                        count++;
-                        list.Add(document._id.Timestamp.ToString(), 
-                            new { currency = document.@base, date = document.date });
-                    }
-                }
-                return new JsonResult(list, new JsonSerializerSettings() { Formatting = Formatting.Indented });
-            }
 
+            FilterDefinitionBuilder<Fixer> builder = Builders<Fixer>.Filter;
+            FilterDefinition<Fixer> filter = builder.Eq(f => f.date, Date.ToString("yyyy-MM-dd")) & builder.Eq(f => f.@base, Base.ToUpper().Substring(0, 3));
+
+            //var count = 0;
+            //using (var cursor = await collection.FindAsync(filter))
+            //{
+            //    while (await cursor.MoveNextAsync())
+            //    {
+            //        var batch = cursor.Current;
+            //        foreach (var document in batch)
+            //        {
+            //            // process document
+            //            count++;
+            //            list.Add(document._id.Timestamp.ToString(),
+            //                new { currency = document.@base, date = document.date });
+            //        }
+            //    }
+            //}
+
+            //return await collection.Find<Fixer>(x => x.date == _date && x.@base == Base.ToUpper()).FirstAsync();
+            //return await collection.Find<Fixer>(filter).First();
+            return await collection.FindAsync<Fixer>(filter).Result.FirstAsync<Fixer>();
+        }
+        [HttpGet("api/Rates/Date/{Date:datetime}/Base/{Base:length(3)}")]
+        public async Task<JsonResult> GetRatesByDateAndBaseAsync(DateTime Date, string Base)
+        {
+            if (Date > DateTime.Now || String.IsNullOrWhiteSpace(Base)) throw new InvalidCastException("[date] or [base] invalid.");
+
+            return new JsonResult(await FirstFixerByDateAndBase(Date, Base), new JsonSerializerSettings() { Formatting = Formatting.Indented });
         }
         [HttpGet("api/Rates")]
-        public JsonResult GetRates()
+        public JsonResult Index()
         {
             return new JsonResult(DataStore.Moc.FixerList);
         }
